61fe704799abfef219c78ca2e772239b62521dc5
leejunghoon <ljh9032a@naver.com>
2018-03-03 09:43:29 +0900

    using decorator

--- plusfriend/views.py
+++ plusfriend/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
+from .decorators import bot
+import json, requests
 
 def keyboard(request):
-
     if request.method == 'GET':
         ret = { "type" : "buttons",
                 "buttons" : ["불 켜기", "불 끄기", "리셋"]
                 }
         return HttpResponse(json.dumps(ret))
 
+
 @csrf_exempt
 def message(request):
-    #print(request.body)
+    print(request.body)
     ret  = {
             'message':{
                 'text':'정상적으로 작동 하였습니다!',
@@ -26,3 +27,21 @@ def message(request):
 
     return HttpResponse(json.dumps(ret))
 
+
+@bot
+def message(request):
+    user_key = request.JSON['user_key']
+    type = request.JSON['type']
+    content = request.JSON['content']
+
+    if type == 'photo':
+        pass
+    else:
+        post = Post.objects.create(user=request.user, message=content)
+        ret  = {
+                'message':{
+                    'text':'저장 되었습니다!'
+                    }
+                }
+        return ret 
+    
